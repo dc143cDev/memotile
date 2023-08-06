5672d0736b59850efa8e6250798e1c177a2a4118
Yejoon kim <devc143c@gmail.com>
2023-08-06 13:53:53 +0900

    gitignore

--- lib/app/modules/home/views/info_view.dart
+++ lib/app/modules/home/views/info_view.dart
@@ -17,7 +17,7 @@ class InfoView extends GetView<HomeController> {
         backgroundColor:
             controller.isDarkModeOn.value == true ? subDark : subLight,
         title: Text(
-          'InfoView',
+          'InfoViw',
           style: TextStyle(
             fontSize: 20,
             fontWeight: FontWeight.w700,
